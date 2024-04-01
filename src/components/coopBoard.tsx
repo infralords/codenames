520d78c43431f5f985f02fa21d9549a7287221be
Hero Protagonist <heroprotagonist32@gmail.com>
2024-04-01 18:12:05 -0300

    add compose single board function

--- src/components/coopBoard.tsx
+++ src/components/coopBoard.tsx
@@ -1,8 +1,27 @@
-import React from "react"
 import Color from "../utils/colors"
 
-class coopBoardUI extends React.Component{
-    constructor(props : [Color[][],Color[][]]){
-        super(props)
-    }
+export function ComposeUIBoard(props : { board : Color[][]}, boardIndex = 0){
+    return (
+        <>
+            {
+                props.board.map((row,i)=>{
+                        return (
+                            <div key={`${boardIndex},${i}`}>
+                                {
+                                    row.map((color,j)=>{
+                                        return <button 
+                                                key   = { `${boardIndex},${i},${j}` } 
+                                                style = {{ backgroundColor: color }}
+                                                className = "gameCard">
+                                                </button>
+                                    })
+                                }
+                            </div>
+                        )
+                    }
+                )
+            }
+        </>
+    )
 }
+
