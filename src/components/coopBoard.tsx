7bc438acfc855e604669cf23a8ff0e93a0e0fb53
Hero Protagonist <heroprotagonist32@gmail.com>
2024-04-01 21:13:38 -0300

    move gameCards to its own component

--- src/components/coopBoard.tsx
+++ src/components/coopBoard.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import Color from "../utils/colors"
+import { ElementOptionSwitch } from "./tristateSwitch/ElementOptionSwitch";
+import { getRandomBoard, getGenerator, getRandomSeed } from "../logic/generate";
 
 export function Board(props : { board : Color[][]}, boardIndex = 0){
     return (
@@ -10,11 +12,12 @@ export function Board(props : { board : Color[][]}, boardIndex = 0){
                             <div key={`${boardIndex},${i}`}>
                                 {
                                     row.map((color,j)=>{
-                                        return <button 
-                                                key   = { `${boardIndex},${i},${j}` } 
-                                                style = {{ backgroundColor: color }}
-                                                className = "gameCard">
-                                                </button>
+                                        return (
+                                            <GameCardButton
+                                                key={`${boardIndex},${i},${j}`}
+                                                color= {color}
+                                            />
+                                        )
                                     })
                                 }
                             </div>
@@ -24,6 +27,19 @@ export function Board(props : { board : Color[][]}, boardIndex = 0){
             }
         </>
     )
+}
+
+export class GameCardButton extends React.Component<{
+    color: Color,
+}>{
+    render(){
+        return (
+            <button 
+                style = {{ backgroundColor: this.props.color}}
+                className = "gameCard">
+            </button>
+        )
+    }
 }
 
 export class CoopGameUI extends React.Component{
@@ -36,20 +52,27 @@ export class CoopGameUI extends React.Component{
     }
     render(){
         return (
-            <div>
+            <div style={{flex: 'auto', justifyContent: 'center'}}>
                 <HideControls />
-                {/* boards */}
+                    <div>
+                        <Board board={ getRandomBoard(getGenerator(getRandomSeed())) }/>
+                    </div>
                 <GamesControls/>
             </div>
         )
     }
 }
 
-export function HideControls(){
-    return (
-        <>
-        </>
-    )
+export class HideControls extends React.Component{
+    constructor(props : {}){
+        super(props)
+    }
+
+    render(){
+        return (
+            <ElementOptionSwitch/>
+        )
+    }
 }
 
 
@@ -58,11 +81,16 @@ export function HideControls(){
  * previous boards, generate new board, next board if it's on a previous board
  */
 
-export function GamesControls(){
-    return (
-        <>
-        </>
-    )
+export class GamesControls extends React.Component{
+    render(){
+        return(
+            <div>
+                <button>previous</button>
+                <button>regenerate</button>
+                <button>next</button>
+            </div>
+        )
+    }
 }
 
 
